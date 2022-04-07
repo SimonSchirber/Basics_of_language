662f879c23c6a2e769af9042859aaf4e292010e4
SimonSchirber <schirber.simon@gmail.com>
2022-04-07 12:25:53 -0700

    java ide with vsc

---
+++ Java/hello.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class hello {
+    //to get the main generate type "main" then click the black square suggested intellesense
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        System.out.println("Hello World");
+    }
+}
