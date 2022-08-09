8a3696c113b759f1e5f7b4eaa33a0aa1b8381468
SimonSchirber <schirber.simon@gmail.com>
2022-08-08 22:41:22 -0700

    c ++ refresher

---
+++ C++/C++_CrashCourse/hello_world.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int my_arr[] = {1,2,3,4,7,12,9};
+
+    int length = sizeof(my_arr)/sizeof(my_arr[0]);
+    cout << "Hello World";
+    return 0;
+}
